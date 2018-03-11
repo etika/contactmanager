c90a7a828aa19c89d8f49c11676dc22234eab7cb
etika ahuja <etika@9piecesof8.com>
2018-03-11 13:52:56 +0530

    Changes in omnicontacts

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,5 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, {:redirect_path => "https://stormy-ravine-32231.herokuapp.com/home/contacts_callback" }
+  importer :gmail, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, {:redirect_path => "/home/contacts_callback" }
 end
