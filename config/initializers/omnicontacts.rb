fcaaa4afa4706e7944b2a61742cd7d0963d7bc66
etika ahuja <etika@9piecesof8.com>
2018-03-11 16:18:11 +0530

    Added ssl_ca_file in initializer

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,5 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, {:redirect_path => "/home/contacts_callback" }
+  importer :gmail, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, {:redirect_path => "/home/contacts_callback", :ssl_ca_file => "/etc/ssl/certs/curl-ca-bundle.crt" }
 end
