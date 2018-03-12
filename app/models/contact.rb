529a1111317798df90d0cfd2b8b2d63df0072392
etika ahuja <etika@9piecesof8.com>
2018-03-12 20:36:59 +0530

    Uncommenting code

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -25,11 +25,10 @@ class Contact < ActiveRecord::Base
   # end
 
   def self.create_contacts(contacts, id)
-    # contacts.each do |contact|
-       contact = contacts.first
+    contacts.each do |contact|
       phone_number = contact[:phone_numbers].map{|contact| contact.values.last} if contact[:phone_numbers].present?
       contact = Contact.create(name: contact[:name],email: contact[:email], phone_number: phone_number, user_id: id)
       contact.addresses.create(lane: 'Anna', city:'new delhi', state: 'chennai',pincode: '1100011', country: 'india')
-    # end
+    end
   end
 end
