a3eea2c3536f9a01109102a5cbebba0f418e8634
etika ahuja <etika@9piecesof8.com>
2018-03-21 22:30:55 +0530

    Changes in models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,17 +2,17 @@ require 'elasticsearch/model'
 class Contact < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
-  self.per_page = 10
 
-  has_many :addresses
+  has_many :addresses, dependent: :destroy
   belongs_to :user
 
-def as_indexed_json(options={})
-  as_json(
-    only: [:email, :name, :phone_number],
-    include: {addresses:{only: [:lane, :city, :state, :country, :pincode]}}
-    )
-end
+  def as_indexed_json(options={})
+    as_json(
+      only: [:email, :name, :phone_number],
+      include: {addresses:{only: [:lane, :city, :state, :country, :pincode]}}
+      )
+  end
+
   def self.create_contacts(contacts, id)
     contacts.each do |contact|
       phone_number = contact[:phone_numbers].map{|contact| contact.values.last} if contact[:phone_numbers].present?
@@ -21,29 +21,3 @@ end
     end
   end
 end
-
-  # def search_data
-  #   {
-  #     email: email
-  #   }
-  # end
-
-  # searchable do
-  #   text :name, :email, :phone_number
-  #   string :phone_number, multiple: true
-  #   text :state do  # this one for full text search
-  #     addresses.map(&:state).compact.join(" ")
-  #   end
-  #   text :city do  # this one for full text search
-  #     addresses.map(&:city).compact.join(" ")
-  #   end
-  #   text :lane do
-  #     addresses.map(&:lane).compact.join(" ")
-  #   end
-  #   text :pincode do
-  #     addresses.map(&:pincode).compact.join(" ")
-  #   end
-  #   text :country do
-  #     addresses.map(&:country).compact.join(" ")
-  #   end
-  # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # searchkick word_start: [:email]
-  # def search_data
-  #   {
-  #     email: email
-  #   }
-  # end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :contacts
+  has_many :contacts, dependent: :destroy
   belongs_to :manager, class_name: "User"
-  has_many :employees, class_name: "User", foreign_key: :manager_id
+  has_many :employees, class_name: "User", foreign_key: :manager_id, dependent: :destroy
 end
